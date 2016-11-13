77bc60dc9db0187d4c3add97bd9fbfb1501c1760
Kersti Perandi <kersti.perandi.001@gmail.com>
2016-11-13 19:51:47 +0200

    programmi tutvustus

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,8 @@
  * Created by Kersti on 13.11.2016.
  */
 public class Main {
+    public static void main(String args[]) {
+        System.out.println("See on helistike õppimise programm");
+        System.out.println("Autor: Kersti");
+    }
 }
